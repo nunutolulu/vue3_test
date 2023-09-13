79e43b3f5de523fe2be6ba25052e5e52bde9acbb
Lyuuu <lllyuwork@163.com>
2023-09-13 20:10:39 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// 引入的不再是Vue构造函数，引入的是一个名为createApp的工厂函数
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
+
+/* const vm = new Vue({
+  render: (h) => h(App),
+});
+vm.$mount("#app"); */
